947677e4ea15a7193e5e7409f8eae31c37e77b9f
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-01-12 17:21:50 +0100

    Remove unnecesary comments

--- src/ejer1/1.js
+++ src/ejer1/1.js
@@ -3,7 +3,6 @@
  Escriba un algoritmo que lea un número entero y determine si es par o impar.
  Si es par, que escriba todos los pares de manera descendiente desde sí mismo y hasta el cero.
  Si es impar, que escriba todos los impares de manera descendiente desde si sí mismo hasta el uno.
- Utilice la instrucción LEER NUMERO al inicio del programa para cargar un número en la variable NUMERO.
 
  */
 
--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ const evenNumber = 20
 const oddNumber = 21
 console.log(`El número ${evenNumber} es ${evenOrOdd(evenNumber).evenOrOdd === 'even' ? 'par' : 'impar'} y por lo tanto su secuencia es: ${evenOrOdd(evenNumber).sequence}`)
 console.log(`El número ${oddNumber} es ${evenOrOdd(oddNumber).evenOrOdd === 'odd' ? 'impar' : 'par'} y por lo tanto su secuencia es: ${evenOrOdd(oddNumber).sequence}`)
+
 console.log('\n------ Ejercicio 2 ------')
 const people = generatePeople()
 const classification = classifyPeople(people)
 print(classification)
+
 console.log('\n------ Ejercicio 3 ------\n')
 console.log(`El sueldo de una persona que trabaja 30 horas a 10€/hora es de ${computeSalary(10, 30)}€`)
 console.log(`El sueldo de una persona que trabaja 40 horas a 10€/hora es de ${computeSalary(10, 40)}€`)
 console.log(`El sueldo de una persona que trabaja 50 horas a 10€/hora es de ${computeSalary(10, 50)}€`)
+
 console.log('\n------------------------')
